730d34f49e9d0f8c066ba913b97568e2bda490b0
chaiyu <chaiyu2002@gmail.com>
2018-01-24 18:10:54 +0800

    see #1306_1

--- src/main/java/com/pinganzhiyuan/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/com/pinganzhiyuan/controller/handler/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.pinganzhiyuan.controller.handler;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.pinganzhiyuan.util.ResponseUtil;
+import com.pinganzhiyuan.util.ResponseBody;
 
 
 /**
@@ -15,14 +17,15 @@ import com.pinganzhiyuan.util.ResponseUtil;
 * @author x1ny
 * @date 2017年5月9日
 */
-//@ControllerAdvice
-//@ResponseBody
-//public class GlobalExceptionHandler {
-//	private final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
-//
-//	@ExceptionHandler(Exception.class)
-//	public String handler(Exception exception) {
-//		logger.error(exception);
-//		return ResponseUtil.standardResponse(new String[]{"发生了未知异常"}, null);
-//	}
-//}
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	private final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handler(Exception exception) {
+		logger.error(exception);
+		ResponseBody resBody = new ResponseBody<>();
+		resBody.statusMsg = "服务端发生错误";
+		return ResponseEntity.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR).body(resBody);
+	}
+}
